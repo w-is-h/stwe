f53c2bec2d0ab4dabace46b5d0942e652fea6e16
w-is-h <w.kraljevic@gmail.com>
2018-06-06 12:44:14 +0000

    Python version and organization of utils

--- stwe/io_utils.py
+++ stwe/io_utils.py
@@ -1,24 +1,12 @@
-from nltk.corpus import stopwords
-import codecs
 import numpy as np
 import time
 from datetime import datetime
-import re
 import operator
 from gensim.models import Word2Vec
-from utils import get_logger
+from .utils import get_logger
 
 log = get_logger("io_utils")
 
-class word2vecIterator(object):
-    def __init__(self, path):
-        self.path = path
-        self.preproc = Preprocessing()
-
-    def __iter__(self):
-        for line in open(self.path):
-            parts = line.split("\t")
-            yield [x.strip() for x in parts[1].split(" ")]
 
 class DocumentTimePair(object):
     def __init__(self, path, time_transform=0, start_time=0, s_prob=1):
@@ -38,91 +26,6 @@ class DocumentTimePair(object):
                     yield ( float(parts[0]), self.preproc.pp_doc(parts[1]) )
                 except:
                     continue
-
-
-class Preprocessing(object):
-    def __init__(self, doc_min_len=6):
-        self.stopwords = stopwords.words('english')
-        self.doc_min_len = doc_min_len
-
-        self.re_nonan = re.compile(r"[^a-zA-Z0-9_'\- ]")
-        self.re_atx = re.compile(r"@\w+")
-        self.re_hashtag = re.compile(r"#\w+")
-        self.re_wspace = re.compile(r"[ ]+")
-        self.re_web = re.compile(r"(?:http:|www)+[\w\-/\.]*")
-        self.re_num = re.compile(r"(?:[^a-z]+\d[^a-z]+)")
-
-    def pp_doc(self, doc):
-        return [x.strip() for x in doc.split() if x not in self.stopwords and len(x) > self.doc_min_len]
-
-    def process_tweet(self, tweet):
-        tweet = tweet.lower()
-        tweet = re.sub(self.re_web, " ", tweet)
-        tweet = re.sub(self.re_atx, " ", tweet)
-        tweet = re.sub(self.re_num, " ", tweet)
-        tags = re.findall(self.re_hashtag, tweet)
-        tags = [x.replace("#", "").strip() for x in tags]
-        tweet = re.sub(self.re_hashtag, " ", tweet)
-        tweet = re.sub(self.re_nonan, " ", tweet)
-        tweet = re.sub(self.re_wspace, " ", tweet)
-        tweet = " ".join([word.strip() for word in tweet.split() if word not in self.stopwords and len(word) > 2])
-
-        return (tags, tweet.strip())
-
-
-def tweets_to_sentences(in_file, out_file_tweets, out_file_tags, offset=4000):
-    """Converts SNAP tweets to senteces and writes everything to
-    out_file_tweets. Creates one additional file containing hashtags from tweets.
-    Each line in the out_tags_file corresponds to the exact line in out_file_tweets.
-
-    in_file: file with tweets in SNAP format
-    out_file_tweets: file where the tweets in the new format will be written
-    out_file_tags: separate file containing hashtags from tweets
-    offset: amount of tweets to skip from the beginning
-
-    Output format is: <timestamp>\t<sentence>
-    """
-    out_tweets = open(out_file_tweets, 'w')
-    out_tags = open(out_file_tags, 'w')
-
-    # Counter for the amount of tweets processed until now
-    t_counter = 0
-
-    pp = Preprocessing()
-    with open(in_file) as data:
-        #Skip first offset lines, usually the beginning has time messed up
-        for i in range(offset):
-            next(data)
-
-        #Find next blank line
-        while True:
-            a = next(data)
-            if a == "\n":
-                break
-
-        t = None
-        for line in data:
-            if line[0] == "T":
-                t = time.mktime(datetime.strptime((" ".join(line.split()[1:])).strip(), "%Y-%m-%d %H:%M:%S").timetuple())
-            elif line[0] == "W":
-                #Use only tweets with > 10 words
-                if len(line.split(" ")) > 10:
-                    if t_counter % 100000 == 0:
-                        print("{} : {} - Tweets processed".format(in_file, t_counter))
-                    t_counter += 1
-
-                    (tags, c_tweet) = pp.process_tweet(" ".join(line.split()[1:]))
-                    # Processed must be longer than 8
-                    if len(c_tweet.split(" ")) > 8:
-                        out.write("{}\t{}\n".format(int(t), c_tweet))
-
-                        if len(tags) > 0:
-                            out_tags.write("{}\n".format(" ".join(tags)))
-                        else:
-                            out_tags.write("{}\n".format("__NONE__"))
-    out.close()
-    out_tags.close()
-
 
 
 def read_clst_file(in_file):
